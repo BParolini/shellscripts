6fb8793f4bdd5ec20982d3ef76847eb481319864
Bruno Parolini <brunoparolini@gmail.com>
2023-04-13 16:00:23 -0300

    Added Rust lang install script

---
+++ dev/installs/rust.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# author: Bruno Parolini brunoparolini@gmail.com
+# description: Installs Rust development libraries
+
+echo "Installing Rust and libraries"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
