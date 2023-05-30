8b4f258b4652d2650969e6b661921767af7e5e59
Bruno Parolini <bruno.parolini@olxbr.com>
2023-05-30 11:23:32 -0300

    Added autocomplete config to rust install script

--- dev/installs/rust.sh
+++ dev/installs/rust.sh
@@ -6,3 +6,6 @@
 echo "Installing Rust and libraries"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+rustup completions zsh > ~/.zfunc/_rustup
+rustup completions zsh cargo > ~/.zfunc/_cargo
