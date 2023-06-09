2e3cf92e94f85e232c9550caf51ef9b88c38df85
Bruno Parolini <brunoparolini@gmail.com>
2023-06-08 21:47:18 -0300

    Added tiling window managers

---
+++ home/installs/tiling.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# author: Bruno Parolini brunoparolini@gmail.com
+# description: Installs i3 and Sway
+
+sudo dnf groupinstall "i3 desktop"
+sudo dnf groupinstall "Sway Desktop"
+
+dnf install fontawesome5-fonts-all
+
