bf96e856a670a41ab00406bdd2199aa1a598a8c0
Bruno Parolini <brunoparolini@gmail.com>
2023-08-06 13:54:28 -0300

    Added Nintendo64 emulator to install script

--- home/installs/emulators.sh
+++ home/installs/emulators.sh
@@ -3,6 +3,11 @@
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Installs various emulators
 
-flatpak install -y --or-update org.ppsspp.PPSSPP net.rpcs3.RPCS3 io.mgba.mGBA org.citra_emu.citra com.github.AmatCoder.mednaffe
+flatpak install -y --or-update org.ppsspp.PPSSPP \
+                               net.rpcs3.RPCS3 \
+                               io.mgba.mGBA \
+                               org.citra_emu.citra \
+                               com.github.AmatCoder.mednaffe \
+                               flathub io.github.simple64.simple64
 
 dnf install -y pcsxr pcsx2 dolphin-emu snes9x snes9x-gtk mupen64plus desmume
