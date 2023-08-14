0b00b9321310eb0b4d3605f8c1a630ecc93a05f8
Bruno Parolini <brunoparolini@gmail.com>
2023-08-13 21:48:08 -0300

    Multiple adjustments

--- dev/installs/sdkman.sh
+++ dev/installs/sdkman.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
 
 # author: Bruno Parolini brunoparolini@gmail.com
-# description: Installs SDKMan, changes some configuration and installs various packages
+# descriptiosn: Installs SDKMan, changes some configuration and installs various packages
 
 curl -s "https://get.sdkman.io" | sh
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-sed -i 's/\(sdkman_auto_answer=\)false/\1true/ \
-        s/\(sdkman_auto_selfupdate=\)false/\1true/' "$HOME/.sdkman/etc/config"
 
 APPS=( "maven" "gradle" "springboot" "kotlin" "groovy" )
 
