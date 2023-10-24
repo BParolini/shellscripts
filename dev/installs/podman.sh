6c3a27fffa2b00d15fc01fb56c45bed867a3ec41
Bruno Parolini <bruno.parolini@olxbr.com>
2023-10-23 22:31:38 -0300

    Added extra configuration for podman installation

--- dev/installs/podman.sh
+++ dev/installs/podman.sh
@@ -5,3 +5,5 @@
 
 echo "Installing podman"
 dnf install -y podman podman-compose podman-plugins buildah podman-docker podman-compose
+
+ln -s "$XDG_RUNTIME_DIR/podman/podman.sock" "$XDG_RUNTIME_DIR/docker.sock"
