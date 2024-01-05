e7fffaeac309e378614b1c9d41aa552bcc0a0a69
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-04 21:32:29 -0300

    Added godoc to go installs script

--- dev/utils/go_installs.sh
+++ dev/utils/go_installs.sh
@@ -6,3 +6,4 @@
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
+go install golang.org/x/tools/cmd/godoc@latest
