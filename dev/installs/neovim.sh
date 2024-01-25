b9c5d626bcbcd692a79f39134f49607ae5c52e32
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-24 22:10:34 -0300

    Corrected missing items in echo

--- dev/installs/neovim.sh
+++ dev/installs/neovim.sh
@@ -3,5 +3,5 @@
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Installs NeoVIM and extensions
 
-echo "dnf install -y neovim python3-neovim"
+echo "dnf install -y neovim python3-neovim ripgrep fd-find"
 dnf install -y neovim python3-neovim ripgrep fd-find
