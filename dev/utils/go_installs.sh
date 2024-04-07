b596e211adac1acb8dd3c33c62916032a6dab07f
Bruno Parolini <bruno.parolini@olxbr.com>
2024-04-06 23:36:32 -0300

    Included golines install

--- dev/utils/go_installs.sh
+++ dev/utils/go_installs.sh
@@ -10,6 +10,7 @@ go install github.com/go-delve/delve/cmd/dlv@latest
 go install golang.org/x/tools/cmd/godoc@latest
 go install github.com/cosmtrek/air@latest
 go install github.com/a-h/templ/cmd/templ@latest
+go install github.com/segmentio/golines@latest
 
 # Extra apps
 go install github.com/jesseduffield/lazygit@latest
