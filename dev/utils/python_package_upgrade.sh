6fcec99b79e789a6a38ca75d00814e411da15df3
Bruno Parolini <bruno.parolini@olxbr.com>
2025-02-13 15:17:49 -0300

    Included extra option

--- dev/utils/python_package_upgrade.sh
+++ dev/utils/python_package_upgrade.sh
@@ -9,4 +9,4 @@ then
     exit 1
 fi
 
-"$1" -m pip list --outdated | awk '{print $1}' | grep -v Package | grep -Ev -- "^-+" | xargs -n1 "$1" -m pip install -U
+"$1" -m pip list --outdated | awk '{print $1}' | grep -v Package | grep -Ev -- "^-+" | xargs -n1 "$1" -Im pip install -U
